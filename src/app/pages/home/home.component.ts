8883b95883539087a408b84c6266f479971453c3
jose.vitor <jose@easyteam.rocks>
2022-02-27 14:56:09 -0300

    remocao comentarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,31 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  // public escola: any = [
-  //   {
-  //     nome: "costa rego",
-  //     nivel: "medio"
-  //   },
-  //   {
-  //     nome: "pedro correia",
-  //     nivel: "fundamental"
-  //   }  
-  // ]
-
-  // public items = [
-  //   {
-  //     label: 'Menu',
-  //     items: [
-  //         [
-  //             {
-  //                 label: 'Escolas',
-  //                 items: [{label: 'Cadastar'}, {label: 'Listar'}]
-  //             }
-  //         ]
-  //     ]        
-  //   }
-  // ]  
-
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-// import {InputTextModule} from 'primeng/inputtext';
-// import {TableModule} from 'primeng/table';
 import {MegaMenuModule} from 'primeng/megamenu';
 import { RegisterSchoolModule } from '../register-school/register-school.module';
 import { ListSchoolModule } from '../list-school/list-school.module';
@@ -13,8 +11,6 @@ import { ListSchoolModule } from '../list-school/list-school.module';
   ],
   imports: [
     CommonModule,
-    // InputTextModule,
-    // TableModule,
     RegisterSchoolModule,
     MegaMenuModule,
     ListSchoolModule
